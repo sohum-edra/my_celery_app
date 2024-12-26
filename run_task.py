ea050609c164ec8af2694355b6d8c94e969a3c02
Sohum Khot <sohum@edralabs.com>
2024-12-26 14:38:22 +0530

    comment await

--- run_task.py
+++ run_task.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     # Call the 'add' task
     result = add.delay(4, 6)
     print(f"Task ID: {result.id}")
-    print(f"Result of add task: {result.get(timeout=10)}")  # Wait for the task to complete
+    # print(f"Result of add task: {result.get(timeout=10)}")  # Wait for the task to complete
 
     # Call the 'multiply' task
     result = multiply.delay(3, 7)
     print(f"Task ID: {result.id}")
-    print(f"Result of multiply task: {result.get(timeout=10)}")  # Wait for the task to complete
+    # print(f"Result of multiply task: {result.get(timeout=10)}")  # Wait for the task to complete
