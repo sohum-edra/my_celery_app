905064a6ec3c92a27154810080d5d811c2977458
Sohum Khot <sohum@edralabs.com>
2024-12-26 14:41:58 +0530

    cleanup

--- run_task.py
+++ run_task.py
@@ -2,11 +2,15 @@ from app.tasks import add, multiply
 
 if __name__ == "__main__":
     # Call the 'add' task
-    result = add.delay(4, 6)
-    print(f"Task ID: {result.id}")
+
+    for idx in range(1, 11):
+        result = add.delay(4, 6)
+        print(f"Invocation {idx} Task ID: {result.id}")
+    # result = add.delay(4, 6)
+    # print(f"Task ID: {result.id}")
     # print(f"Result of add task: {result.get(timeout=10)}")  # Wait for the task to complete
 
     # Call the 'multiply' task
-    result = multiply.delay(3, 7)
-    print(f"Task ID: {result.id}")
+    # result = multiply.delay(3, 7)
+    # print(f"Task ID: {result.id}")
     # print(f"Result of multiply task: {result.get(timeout=10)}")  # Wait for the task to complete
