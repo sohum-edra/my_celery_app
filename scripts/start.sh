96f1df10c84a1a01f921ff9a1993dffd7439cfd3
Sohum Khot <sohum@edralabs.com>
2024-12-27 13:15:13 +0530

    run celery in bg

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Navigate to your application directory
 cd /home/ec2-user/my_celery_app
-# Example of using Poetry environment:
-poetry run celery -A app worker --loglevel=info
+
+# Activate the Poetry environment (if necessary)
+# source /home/ec2-user/my_celery_app/.venv/bin/activate
+
+# Start Celery in the background
+nohup poetry run celery -A app worker --loglevel=info > celery.log 2>&1 &
+
+# Optionally, you can output the PID to a file
+echo $! > celery.pid
